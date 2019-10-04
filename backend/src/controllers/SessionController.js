ac5d718ace57912139d84f83c35e8c001737e9e2
Cristian <cristian123105@gmail.com>
2019-10-04 11:50:55 -0300

    add rota para trazer a thumbnail

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -5,8 +5,9 @@ module.exports = {
         const {email} = request.body;
         
         let user = await User.findOne({ email });
+
         if(!user){
-             user = await User.create({ email })
+             user = await User.create({ email });
         }
         
         return response.json(user);
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const path = require('path')
 
 mongoose.connect('mongodb+srv://cristuker:136crcc12@aircnc-gszt9.mongodb.net/aircnc?retryWrites=true&w=majority',{
     useNewUrlParser: true,
@@ -15,7 +16,7 @@ mongoose.connect('mongodb+srv://cristuker:136crcc12@aircnc-gszt9.mongodb.net/air
 //request.body = Acessar corpo da requisição(criação edicção de registro)
 app.use(cors());
 app.use(express.json());
-
+app.use('/files',express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 app.listen(3333);
