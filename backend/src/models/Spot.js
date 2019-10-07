2656954213417b5010f1140ebfe3c3d7c0732651
Cristian <cristian123105@gmail.com>
2019-10-07 09:56:03 -0300

    add websocket e rotas para aprovação e rejeição

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -16,7 +16,7 @@ const SpotSchema = new mongoose.Schema({
 });
 
 SpotSchema.virtual('thumbnail_url').get(function(){
-    //return `http://localhost:3333/files/${this.thumbnail}`
-    return `http://192.168.0.107:3333/files/${this.thumbnail}`
+    return `http://localhost:3333/files/${this.thumbnail}` // para retornar dados no react
+    //return `http://192.168.0.107:3333/files/${this.thumbnail}` // para retorna dados no mobile utilizando o expo `http://ip do expo:3333/files/${this.thumbnail}`
 })
 module.exports = mongoose.model('Spot', SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,39 @@
 const express = require('express');
-const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require('./routes');
+
 const app = express();
-const path = require('path')
+
+const server = http.Server(app);
+const io = socketio(server);
+
+
 
 mongoose.connect('mongodb+srv://cristuker:136crcc12@aircnc-gszt9.mongodb.net/aircnc?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+ app.use((request, response, next) =>{
+    request.io = io;
+    request.connectedUsers = connectedUsers;
+
+    return next();
+ })
 //get, post , put, delete
 //request.query ou req.query = Acessar query params (para filtros)
 //request .params = Acessar route params (para edição,delete) put
@@ -19,5 +43,5 @@ app.use(express.json());
 app.use('/files',express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
 
